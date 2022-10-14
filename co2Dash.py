d9d137d39a3c799a720a1c15540778d9772ff0d6
jawook <91631714+jwilkmarquee@users.noreply.github.com>
2022-10-14 14:55:16 -0600

    Update co2Dash.py

--- co2Dash.py
+++ co2Dash.py
@@ -52,4 +52,5 @@ yrDf = getdf(sourcedf, selYr, selMinPop)
 
 chrBarPerCap = px.bar(yrDf, x='country', y='co2_per_capita', 
                       color_discrete_sequence=clrs)
+chrBarPerCap.update_xaxes(categoryorder='total descending')
 st.plotly_chart(chrBarPerCap)
