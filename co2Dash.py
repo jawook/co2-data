cd1b90183332888f280a6fe2ea17393b8856d6ff
jawook <jamie.c.wilkie@gmail.com>
2022-10-14 16:50:39 -0600

    Update co2Dash.py

--- co2Dash.py
+++ co2Dash.py
@@ -63,7 +63,7 @@ chrBarPerCap = px.bar(yrDf, x='country', y='co2_per_capita', color='topN',
                       color_discrete_sequence=clrs)
 chrBarPerCap.update_xaxes(categoryorder='total descending', title=None)
 chrBarPerCap.update_yaxes(title='Total CO<sub>2</sub> Emissionse per Capita',
-                          tickformat='.2f')
+                          tickformat='.0f')
 chrBarPerCap.update_layout(margin={'l': 0, 't': 0, 'r': 0, 'b':0}, 
                            paper_bgcolor='rgba(0,0,0,0)',
                            plot_bgcolor='rgba(0,0,0,0)',
